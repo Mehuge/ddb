f12c49352e413fcfde9e2d60fa58ec32f873b001
Mehuge <adf@sorcerer.co.uk>
2026-08-14 17:21:22 +0100

    Bump version to 1.0.0-beta.20

--- ddb.js
+++ ddb.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const DDB_VERSION = '1.0.0-beta.19';
+const DDB_VERSION = '1.0.0-beta.20';
 
 // A map of command names to their module paths for clarity and easy maintenance.
 const commands = {
